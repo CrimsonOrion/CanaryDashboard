15e2bfb64600b5f16deb7876448101f37d88c1ba
JamesDLynch <jiml@ccemc.com>
2022-03-04 14:54:20 -0500

    Added Shortcuts for Known Region/View Names

---
+++ Canary Dashboard.Core/KnownRegionNames.cs
@@ -0,0 +1,6 @@
+﻿namespace Canary_Dashboard.Core;
+public static class KnownRegionNames
+{
+    public static string MainRegion => "MainRegion";
+    public static string DetailsRegion => "DetailsRegion";
+}
---
+++ Canary Dashboard.Core/KnownViewNames.cs
@@ -0,0 +1,8 @@
+﻿namespace Canary_Dashboard.Core;
+public static class KnownViewNames
+{
+    public static string CanaryDataGridView => "CanaryDataGridView";
+    public static string CanaryFolderDetailView => "CanaryFolderDetailView";
+    public static string CanaryDocxDetailView => "CanaryDocxDetailView";
+    public static string CanaryXlsxDetailView => "CanaryXlsxDetailView";
+}
